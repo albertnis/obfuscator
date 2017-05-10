c40b2706f7c1643a87db2cfff244654d5df72599
albertnis <albertnis@gmail.com>
2017-05-10 23:24:36 +1200

    Better submit button UX when loading and cleaner output formatting

--- js/obfuscator.js
+++ js/obfuscator.js
@@ -2,13 +2,26 @@ $(function() {
     $("select.lang").droppy();
 
     $(".do-obfuscate").on("click", function() {
+        var langs = [$("#lang-0").val(), $("#lang-1").val(), $("#lang-2").val(), $("#lang-3").val()];
+        $(".do-obfuscate").html("Loading");
+        $(".do-obfuscate").prop("disabled", true);
+        $(".do-obfuscate").addClass("loading");
         $.ajax({
-          method: "GET",
+          method: "POST",
           url: "php/translator.php",
-          data: { text: $("#obf-str").val(), from: $("#lang-0").val(), a: $("#lang-1").val(), b: $("#lang-2").val(), c: $("#lang-3").val()}
+          data: { text: $("#obf-str").val(), langs: JSON.stringify(langs) }
         }).done(function( msg ) {
-            $(".row-output").html(msg);
+            $(".do-obfuscate").html("Submit");
+            $(".do-obfuscate").prop("disabled", false);
+            $(".do-obfuscate").removeClass("loading");
             $(".row-output").fadeIn(300);
+            $(".row-output").empty();
+
+            var ro = JSON.parse(msg);
+            for (var i = 0; i < ro.length; i++) {
+                var elem = '<div><div class="langtip">['+ro[i]["lang"]+']</div>'+ro[i]["text"]+'</div>';
+                $(".row-output").append(elem);
+            }
         });
     });
 });
