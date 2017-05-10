6b7a711102f16863f6ed4511805a3a4c800f748d
albertnis <albertnis@gmail.com>
2017-05-10 23:07:10 +1200

    Imported classes and functions from MS translator HTTP examples

---
+++ php/mstapi.php
@@ -0,0 +1,112 @@
+<?php
+class AccessTokenAuthentication {
+    /*
+     * Get the access token.
+     *
+     * @param string $azure_key    Subscription key for Text Translation API.
+     *
+     * @return string.
+     */
+    function getToken($azure_key)
+    {
+        $url = 'https://api.cognitive.microsoft.com/sts/v1.0/issueToken';
+        $ch = curl_init();
+        $data_string = json_encode('{body}');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen($data_string),
+            'Ocp-Apim-Subscription-Key: ' . $azure_key
+        )
+                   );
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        $strResponse = curl_exec($ch);
+        if ($strResponse === false) {
+            throw new Exception(curl_error($ch), curl_errno($ch));
+        }
+        curl_close($ch);
+        return $strResponse;
+    }
+}
+/*
+ * Class:HTTPTranslator
+ *
+ * Processing the translator request.
+ */
+Class HTTPTranslator {
+    /*
+     * Create and execute the HTTP CURL request.
+     *
+     * @param string $url        HTTP Url.
+     * @param string $authHeader Authorization Header string.
+     * @param string $postData   Data to post.
+     *
+     * @return string.
+     *
+     */
+    function curlRequest($url, $authHeader) {
+        //Initialize the Curl Session.
+        $ch = curl_init();
+        //Set the Curl url.
+        curl_setopt ($ch, CURLOPT_URL, $url);
+        //Set the HTTP HEADER Fields.
+        curl_setopt ($ch, CURLOPT_HTTPHEADER, array($authHeader,"Content-Type: text/xml"));
+        //CURLOPT_RETURNTRANSFER- TRUE to return the transfer as a string of the return value of curl_exec().
+        curl_setopt ($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        //CURLOPT_SSL_VERIFYPEER- Set FALSE to stop cURL from verifying the peer's certificate.
+        curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, False);
+        //Execute the  cURL session.
+        $curlResponse = curl_exec($ch);
+        //Get the Error Code returned by Curl.
+        $curlErrno = curl_errno($ch);
+        if ($curlErrno) {
+            $curlError = curl_error($ch);
+            throw new Exception($curlError);
+        }
+        //Close a cURL session.
+        curl_close($ch);
+        return $curlResponse;
+    }
+}
+
+/*
+ * Create and execute the HTTP CURL request.
+ *
+ * @param string $url        HTTP Url.
+ * @param string $authHeader Authorization Header string.
+ * @param string $postData   Data to post.
+ *
+ * @return string.
+ *
+ */
+function curlRequest($url, $authHeader, $postData=''){
+    //Initialize the Curl Session.
+    $ch = curl_init();
+    //Set the Curl url.
+    curl_setopt ($ch, CURLOPT_URL, $url);
+    //Set the HTTP HEADER Fields.
+    curl_setopt ($ch, CURLOPT_HTTPHEADER, array($authHeader,"Content-Type: text/xml"));
+    //CURLOPT_RETURNTRANSFER- TRUE to return the transfer as a string of the return value of curl_exec().
+    curl_setopt ($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    //CURLOPT_SSL_VERIFYPEER- Set FALSE to stop cURL from verifying the peer's certificate.
+    curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, False);
+    if($postData) {
+        //Set HTTP POST Request.
+        curl_setopt($ch, CURLOPT_POST, TRUE);
+        //Set data to POST in HTTP "POST" Operation.
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+    }
+    //Execute the  cURL session.
+    $curlResponse = curl_exec($ch);
+    //Get the Error Code returned by Curl.
+    $curlErrno = curl_errno($ch);
+    if ($curlErrno) {
+        $curlError = curl_error($ch);
+        throw new Exception($curlError);
+    }
+    //Close a cURL session.
+    curl_close($ch);
+    return $curlResponse;
+}
